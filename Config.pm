0fb13a9df14d4f9f2b4ac8ed2c92556540247304
Stephen Jayna <ext-stephen.jayna@nokia.com>
2011-03-29 10:42:23 +0100

    Changes to make this ready for use in production environment.

--- Config.pm
+++ Config.pm
@@ -1,15 +1,12 @@
 # -*- Mode: perl; indent-tabs-mode: nil -*-
 
-
 package Bugzilla::Extension::MediaInfo;
 use strict;
 
 use constant NAME => 'MediaInfo';
 
-use constant REQUIRED_MODULES => [
-];
+use constant REQUIRED_MODULES => [];
 
-use constant OPTIONAL_MODULES => [
-];
+use constant OPTIONAL_MODULES => [];
 
 __PACKAGE__->NAME;
--- Extension.pm
+++ Extension.pm
@@ -1,54 +1,55 @@
 # -*- Mode: perl; indent-tabs-mode: nil -*-
 
-
 package Bugzilla::Extension::MediaInfo;
 
-#use strict;
+use strict;
 use base qw(Bugzilla::Extension);
+
 use Bugzilla::Comment;
+
+use File::Copy qw/ copy /;
+use File::Temp qw/ tempfile /;
 use POSIX;
-use File::Copy cp;
 
 our $VERSION = '0.01';
 
-
 sub attachment_process_data {
     my ($self, $args) = @_;
+
     return unless $args->{attributes}->{mimetype} =~ m/^video/;
-   
-    my $data = ${$args->{data}};
-    # $data is a filehandle.
+
+    my $data = ${ $args->{data} };
+    my (undef, $tempfile) = tempfile();
+
     if (ref $data) {
-        cp($data, "/tmp/bug_attachment");
+        # $data is a filehandle.
+        copy($data, $tempfile) || die($!);
     }
-    # $data is a blob.
     else {
-        open OUTPUT, ">/tmp/bug_attachment";
+        # $data is a blob.
+        open(OUTPUT, ">$tempfile") || die($!);
         print OUTPUT $data;
         close OUTPUT;
     }
 
-    my $comment = "";
-    if (-e "/usr/bin/mediainfo")
-    {
-        $comment = `/usr/bin/mediainfo /tmp/bug_attachment`;
+    my $comment;
+
+    if (-e "/usr/bin/mediainfo") {
+        $comment = `/usr/bin/mediainfo $tempfile`;
     }
-    elsif (-e "/usr/bin/gst-discoverer-0.10")
-    {
-        $comment = `/usr/bin/gst-discoverer-0.10 --timeout=60 /tmp/bug_attachment`;
+    elsif (-e "/usr/bin/gst-discoverer-0.10") {
+        $comment = `/usr/bin/gst-discoverer-0.10 --timeout=60 $tempfile`;
     }
-    elsif (-e "/usr/bin/file")
-    {
-        $comment = `/usr/bin/file -b /tmp/bug_attachment`;
+    elsif (-e "/usr/bin/file") {
+        $comment = `/usr/bin/file -b $tempfile`;
     }
-    else {
-        # Return without printing additional comment
-        return;
+
+    if ($comment) {
+        my $bug = $args->{attributes}->{bug};
+        $bug->add_comment("Attachment info: " . $comment);
     }
-    my $bug = $args->{attributes}->{bug};
-    $bug->add_comment("Attachment info: " . $comment);
-    unlink '/tmp/bug_attachment';
-  
+
+    unlink($tempfile) || die($!);
 }
 
- __PACKAGE__->NAME;
+__PACKAGE__->NAME;
